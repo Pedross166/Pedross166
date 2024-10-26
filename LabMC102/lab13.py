ea41d191c500ac6bad9c407b523480952a8cc747
Pedross166 <164056610+Pedross166@users.noreply.github.com>
2024-10-26 23:09:17 +0000

    renamed:    LabMC102/Lab01.py -> LabMC102/lab01.py 	renamed:    LabMC102/Lab02.py -> LabMC102/lab02.py     renamed:    LabMC102/Lab03.py -> LabMC102/lab03.py 	renamed:    LabMC102/Lab04.py -> LabMC102/lab04.py 	renamed:    LabMC102/Lab05.py -> LabMC102/lab05.py 	renamed:    LabMC102/Lab06.py -> LabMC102/lab06.py 	renamed:    LabMC102/Lab07.py -> LabMC102/lab07.py 	renamed:    LabMC102/Lab08.py -> LabMC102/lab08.py 	renamed:    LabMC102/Lab09.py -> LabMC102/lab09.py 	renamed:    LabMC102/Lab10.py -> LabMC102/lab10.py 	renamed:    LabMC102/Lab11.py -> LabMC102/lab11.py 	renamed:    LabMC102/Lab12.py -> LabMC102/lab12.py 	new file:   LabMC102/lab13.py 	renamed:    LabMC102/Lab13.py -> LabMC102/lab14.py 	renamed:    LabMC102/Lab14.py -> LabMC102/lab15.py

---
+++ LabMC102/lab13.py
@@ -0,0 +1,23 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 13 - Jogos Olímpicos
+# Nome:
+# RA:
+###################################################
+
+# Leitura da quantidade de provas e pesos
+N, O, P, B = [int(i) for i in input().split()]
+
+# Leitura das provas
+
+# Ordenação pelo Critério Oficial
+print('Critério Oficial:')
+
+# Ordenação Ponderado
+print('Ponderado:')
+
+# Ordenação por Recordes Olímpicos
+print('Recorde Olímpico:')
+
+# Ordenação por Dobradinha
+print('Dobradinha:')
